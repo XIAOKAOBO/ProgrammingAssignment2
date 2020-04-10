dc06452ebba7630a7f3f6f2256e1f59aa894d9b5
Shi Liuyin <cobbshi810@outlook.com>
2020-04-09 21:29:33 -0400

    lol

--- program.R
+++ program.R
@@ -24,3 +24,32 @@ cachemean <- function(x, ...) {
   x$setmean(m)
   m
 }
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+
+cacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("cached data is there")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
